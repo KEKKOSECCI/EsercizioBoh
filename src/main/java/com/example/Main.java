7ba1bbe3e01b3358268accfbdd3db3efbda3cf8a
lai2-24 <lai2-24@itismeucci.com>
2025-11-18 12:38:44 +0100

    Modifiche

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,29 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(3000);
+        do {
+            System.out.println("stiamo cercando un Client");
+            Socket clientSocket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            //facciamo finta che questi messaggi stampati a terminale che servano a me per fare debug poi diventino scritture in file di log per memorizzare le azioni del nostro server
+            System.out.println("Client trovato");
+            do {
+                String get = in.readLine();
+                String[] getAr = get.split("_");
+                System.out.println(getAr[0]);
+                System.out.println(getAr[1]);
+                System.out.println(getAr[2]);
+            } while (true);
+        } while (true);
     }
 }
